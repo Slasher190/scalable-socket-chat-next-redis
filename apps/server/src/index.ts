c3e07a7b5e1772b227cd8f984d37f5722d4a2e82
Sudhi Ranjan Gupta <sudhigupta190@gmail.com>
2023-12-22 01:57:30 +0530

    added socket one-one connection

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -11,6 +11,6 @@ async function init() {
   httpServer.listen(PORT, () =>
     console.log(`HTTP Server started at PORT: ${PORT}`)
   );
+  socketService.initListeners();
 }
-
-init();
+init()
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,12 +4,27 @@ class SocketService {
   private _io: Server;
 
   constructor() {
-    this._io = new Server();
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
     console.log("Initialize socket server...");
   }
 
   public get io() {
     return this._io;
+  }
+  public initListeners() {
+    console.log("Initialize socket listners...");
+    const io = this.io;
+    io.on("connect", (socket) => {
+      console.log(`New Socket Connected ${socket.id}`);
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log(`New message recieved : ${message}`);
+      });
+    });
   }
 }
 
